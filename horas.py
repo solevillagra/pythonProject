daa8481a51fd18d61bb12c1d968b917f547bb9df
soledadvillagra <soledad.m.villagra@gmail.com>
2021-09-08 20:25:02 -0300

    Se actualizó lint

--- horas.py
+++ horas.py
@@ -3,16 +3,16 @@ horaEnSeg = 3600
 segundosPorMinuto= 60
 
 #1- Recibir cantidad de horas
-horas= float (input("Ingrese la cantidad de horas: "))
+horas = float(input("Ingrese la cantidad de horas: "))
 
 #2- Recibir total minutos
-minutos = float (input("Ingrese la cantidad de minutos: "))
+minutos = float(input("Ingrese la cantidad de minutos: "))
 
 #3- Convertir horas en minutos:
 minutosEnHoras = horas * minXhora
 
 #4- Sumar total de minutos
-totalMinutos = minutosEnHoras * minutos
+totalMinutos = minutosEnHoras + minutos
 
 #5- Convertir  totalMinutos en segundos
 totalSegundos = totalMinutos * segundosPorMinuto
